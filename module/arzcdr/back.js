69d03e6f15d2ffb8972954d740e7f7f3b422d756
Ajans Mobil <info@ajansmobil.com>
2026-02-22 23:50:34 +0000

    Edit

--- module/arzcdr/back.js
+++ module/arzcdr/back.js
@@ -1,2 +1,4 @@
+// Dinleyici yorumları ve AI radyo asistanı yanıtları API'den gelir (Kaldırapi/sabit veri kaldırıldı).
+// Modül HTML tarafında LISTENER_COMMENTS_API ve AI_ASSISTANT_API ile endpoint'ler kullanılır.
 console.log('code bos');
 html = html;
