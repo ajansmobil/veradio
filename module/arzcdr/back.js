85e47e57f8692ee1367215f2e49fe58386957159
Ajans Mobil <info@ajansmobil.com>
2026-02-25 20:06:53 +0000

    Edit

--- module/arzcdr/back.js
+++ module/arzcdr/back.js
@@ -1,3 +1,20 @@
-// Chat/chatlist API: modül kuralı gereği index.html'de apiurl = "https://{{domain}}/api" kullanılır (login ile aynı).
-// Küfür içeren yorum gönderilince API 400 döner; ekranda "Küfretmek yasaktır. Lütfen ifadenizi düzeltin." uyarısı gösterilir (arzcdr-profanity-warning).
-html = html;
+langhtml = "";
+var currentPagePath = (page && typeof page === "object" && page.path !== undefined) ? page.path : "";
+for (const key in webmakerdata.setting.langs) {
+  if (webmakerdata.setting.langs[key] === true) {
+    var langUrl = (currentPagePath === "") ? `/${key}/` : `/${key}/${currentPagePath}/`;
+    langhtml += `<li><a href="${langUrl}"><img height="25" src="/src/lang/${key}.png" alt="${key}"></a></li>`;
+  }
+}
+html = html.replace(/{{langhtml}}/g, langhtml);
+
+
+
+let backhtml = "";
+for (const iterator of json.data) {
+  backhtml += `
+        <a href="${iterator["externalUrl"]}" target="_blank" style="text-decoration: none;">
+            <img src="${iterator["img"]}" alt="${iterator["name"]}" class="modulex2-media" />
+        </a>`;
+}
+html = html.replace(new RegExp("{{html}}", "g"), backhtml);
