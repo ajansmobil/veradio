1504f98230c544d2c98d60ce87579e6c32299290
Ajans Mobil <info@ajansmobil.com>
2026-02-25 20:52:21 +0000

    Edit

--- module/arzcdr/back.js
+++ module/arzcdr/back.js
@@ -1,6 +1,6 @@
 langhtml = "";
 var currentPagePath = (page && typeof page === "object" && page.path !== undefined) ? page.path : "";
-for (const key in webmakerdata.setting.langs) {
+for (var key in webmakerdata.setting.langs) {
   if (webmakerdata.setting.langs[key] === true) {
     var langUrl = (currentPagePath === "") ? `/${key}/` : `/${key}/${currentPagePath}/`;
     langhtml += `<li><a href="${langUrl}"><img height="25" src="/src/lang/${key}.png" alt="${key}"></a></li>`;
@@ -10,11 +10,10 @@ html = html.replace(/{{langhtml}}/g, langhtml);
 
 
 
-let backhtml = "";
-for (const iterator of json.data) {
-  backhtml += `
-        <a href="${iterator["externalUrl"]}" target="_blank" style="text-decoration: none;">
-            <img src="${iterator["img"]}" alt="${iterator["name"]}" class="modulex2-media" />
-        </a>`;
+var backhtml = "";
+var i;
+for (i = 0; i < json.data.length; i++) {
+  var iterator = json.data[i];
+  backhtml += "\n        <a href=\"" + iterator["externalUrl"] + "\" target=\"_blank\" style=\"text-decoration: none; cursor: pointer;\">\n            <img src=\"" + iterator["img"] + "\" alt=\"" + iterator["name"] + "\" class=\"modulex2-media\" />\n        </a>";
 }
 html = html.replace(new RegExp("{{html}}", "g"), backhtml);
